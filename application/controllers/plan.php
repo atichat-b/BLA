8053ea580490bb4651eebdaf5777288d971f9be1
Atichat Saetae <atichat.b@gmail.com>
2013-06-20 09:45:17 +0700

    update htaccess and session

--- application/controllers/plan.php
+++ application/controllers/plan.php
@@ -6,12 +6,15 @@ class plan extends CI_Controller {
 
 	public function index() {
 		$this -> load -> view('plan');
-		session_start();
-		$_SESSION['name'] = "Atichat Saetae";
+		//session_start();
+		$userdata=array('name'=>'names');
+		$this->session->set_userdata($userdata);
+		echo $this->session->userdata('name');
+		//$_SESSION['name'] = "Atichat Saetae";
 	}
 	
 	public function test(){
-		session_start();
+		//session_start();
 		echo $_SESSION['name'];
 	}
 
